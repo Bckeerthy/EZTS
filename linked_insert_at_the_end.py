4e3be90d54796c7ce04df64662d0a80df44326fc
Keerthi Reddy <keerthy1603@gmail.com>
2024-07-01 09:50:00 +0530

    linked_insert_at_the_end.py

---
+++ linked_insert_at_the_end.py
@@ -0,0 +1,38 @@
+#inserting at the end
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = Node(data)
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end = " ")
+            current = current.next
+        print()
+
+
+ll = LinkedList()
+
+
+ll.insert_at_end(1)
+ll.insert_at_end(2)
+ll.insert_at_end(3)
+ll.insert_at_end(4)
+ll.insert_at_end(5)
+
+
+ll.print_list()
